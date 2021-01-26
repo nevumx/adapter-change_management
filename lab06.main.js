bc2b3f2babf864125f3b2ebc7d2a352fbd36d684
nevumx <nevumx@gmail.com>
2021-01-26 00:19:20 +0000

    Final Project

--- lab06.main.js
+++ lab06.main.js
@@ -158,27 +158,7 @@ class ServiceNowAdapter extends EventEmitter {
    *   handles the response.
    */
   getRecord(callback) {
-    this.connector.get((data, error) => {
-      if (error) {
-        callback(data, error);
-      } else if (data && data.body) { 
-        const convertedData = [];
-        for (const ticket of JSON.parse(data.body).result) {
-          convertedData.push({
-            change_ticket_number: ticket.number,
-            active: ticket.active,
-            priority: ticket.priority,
-            description: ticket.description,
-            work_start: ticket.work_start,
-            work_end: ticket.work_end,
-            change_ticket_key: ticket.sys_id,
-          });
-        }
-        callback(convertedData, error);
-      } else {
-        callback(data, error);
-      }
-    });
+    this.connector.get(callback);
   }
 
   /**
@@ -191,24 +171,7 @@ class ServiceNowAdapter extends EventEmitter {
    *   handles the response.
    */
   postRecord(callback) {
-    this.connector.post((data, error) => {
-      if (error) {
-        callback(data, error);
-      } else if (data && data.body) {
-        const ticket = JSON.parse(data.body).result;
-        callback({
-          change_ticket_number: ticket.number,
-          active: ticket.active,
-          priority: ticket.priority,
-          description: ticket.description,
-          work_start: ticket.work_start,
-          work_end: ticket.work_end,
-          change_ticket_key: ticket.sys_id,
-        }, error);
-      } else {
-        callback(data, error);
-      }
-    });
+    this.connector.post(callback);
   }
 }
 
